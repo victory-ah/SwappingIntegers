0638e888cbfd9f216f77f013a8c7c22d6d1196d7
victory-ah <52970645+victory-ah@users.noreply.github.com>
2019-08-01 10:28:16 -0700

    Add files via upload

---
+++ SwapInt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntSwap
+{
+    class SwapInt
+    {
+        public static string Swap(int A, int B)
+        {
+            B = A + B;
+            A = B - A;
+            B = B - A;
+
+            return $"{A}, {B}";
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IntegerSwap.Main()");
+        }
+    }
+}
